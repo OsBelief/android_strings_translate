40211eeee10ab58b0acad3b536d91e06ab381eeb
chengzhenhua <chengzhenhua@xylink.com>
2019-06-18 15:13:30 +0800

    重新创建文件, 写入xml

--- baidu_translate.py
+++ baidu_translate.py
@@ -1,14 +1,15 @@
 # /usr/bin/env baidu_translate
 # coding=utf8
 
-import http.client  # 修改引用的模块
+import http.client
 from hashlib import md5
 import urllib
 import random
 from xml.etree import ElementTree as ET
 import json as js
+import os
 
-filePath = "/Users/colorful/Desktop/ME20_40简体中文_繁体中文/cameracontrol/strings_tw.xml"
+filePath = "/Users/colorful/Desktop/ME20_40简体中文_繁体中文/cameracontrol/strings.xml"     # 设置strings.xml的路径
 
 appid = '20190617000308218'  # 你的appid
 secretKey = 'ntwK9usqie7s0ojom0gP'  # 你的密钥
@@ -22,6 +23,8 @@ salt = random.randint(32768, 65536)
 ET.register_namespace('tools', "http://schemas.android.com/tools")
 tree = ET.parse(filePath)
 root = tree.getroot()
+
+result = True
 
 for child in root:
     sign = appid + child.text + str(salt) + secretKey
@@ -48,15 +51,23 @@ for child in root:
         child.text = trans_result[0]["dst"]
 
     except Exception as e:
+        result = False
         print("请求百度翻译接口失败！", e.args)
         print("翻译失败-------", child.text)
     finally:
         if httpClient:
             httpClient.close()
 
-xml_str = '<?xml version="1.0" encoding="UTF-8"?>\n' + ET.tostring(root, method='xml', encoding="UTF-8").decode("utf-8")  # 避免声明是单引号
+if result is False:
+    print("存在翻译失败的字符串, 退出程序!")
+    exec(0)
+
+xml_str = '<?xml version="1.0" encoding="UTF-8"?>\n' + ET.tostring(root, method='xml', encoding="UTF-8").decode(
+    "UTF-8")  # 避免声明是单引号
 print(xml_str)
-with open(filePath, 'w') as xml_file:
+targetFile = os.path.dirname(filePath) + "/strings_" + toLang + ".xml"
+
+with open(targetFile, 'w') as xml_file:
     xml_file.write(xml_str)
 
 print("-----XML字段更新完毕！")
